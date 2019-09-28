53c2805e2d8fe5f43a9877909130d325ba44bb33
Maddie <maddiemcampbell@gmail.com>
2019-09-28 14:08:54 -0700

    basic to do list state not setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      userList: [],
+      selectedTask: [],
+    }
+  }
 
   render() {
     return (
       <div className='container'>
-
+        <h1>To Do List</h1>
+        <ul>
+          <li>{this.state.userList}</li>
+        </ul>
+        <button>Add New Task</button>
       </div>
     );
   }
